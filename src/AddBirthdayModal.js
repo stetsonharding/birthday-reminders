ea6569ac98cb107bc09903ec29ed80d7ba3cd49c
= <stetson2001@msn.com>
2022-09-03 10:09:19 -0700

    add birthday inputs

--- src/AddBirthdayModal.js
+++ src/AddBirthdayModal.js
@@ -1,21 +1,31 @@
 import React from 'react'
 import "./AddBirthdayModal.css"
 
-export default function AddBirthdayModal({isAddBirthdayShown, setIsAddBirthdayShown}) {
+export default function AddBirthdayModal({ isAddBirthdayShown, setIsAddBirthdayShown }) {
 
-const showHideClassName = isAddBirthdayShown ? 'modal-container display-block' : 'modal display-none'
+    const showHideClassName = isAddBirthdayShown ? 'modal-container display-block' : 'modal display-none'
 
-    return(
+    return (
         <div className={showHideClassName}>
-            <div className="modal">
-                <p className="modal-title">Who's Birthday?</p>
-                <form className="birthday-form">
-                    
-                </form>
-                <button onClick={() => setIsAddBirthdayShown(false)}>Close</button>
+            <div className='modal'>
+                <p className='modal-title'>Who's Birthday?</p>
+                <div className='form-container'>
+                    <form className='birthday-form'>
+
+                        <input type='text' placeholder='Name' />
+                        <br />
+                        <input type='text' placeholder='Age' />
+                        <br />
+                        <label for="myfile">Select a Image: </label>
+                        <input type="file" id="myfile" name="myfile" />
+                        <br />
+                        <button onClick={() => setIsAddBirthdayShown(false)}>Close</button>
+                    </form>
+                </div>
+
             </div>
         </div>
-        
+
     )
 
 }
