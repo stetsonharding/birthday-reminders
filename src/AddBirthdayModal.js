76a81bec9be1accdc24ec4165712a89d137273dc
stetsonharding <stetson2001@msn.com>
2022-09-03 19:21:41 -0700

    modal inputs

--- src/AddBirthdayModal.js
+++ src/AddBirthdayModal.js
@@ -1,31 +1,40 @@
-import React from 'react'
-import "./AddBirthdayModal.css"
+import React from "react";
+import "./AddBirthdayModal.css";
 
-export default function AddBirthdayModal({ isAddBirthdayShown, setIsAddBirthdayShown }) {
+function AddBirthdayModal({ setIsAddBirthdayShown, isAddBirthdayShown }) {
+  const showHideClassName = isAddBirthdayShown
+    ? "modal-container display-block"
+    : "modal display-none";
 
-    const showHideClassName = isAddBirthdayShown ? 'modal-container display-block' : 'modal display-none'
-
-    return (
-        <div className={showHideClassName}>
-            <div className='modal'>
-                <p className='modal-title'>Who's Birthday?</p>
-                <div className='form-container'>
-                    <form className='birthday-form'>
-
-                        <input type='text' placeholder='Name' />
-                        <br />
-                        <input type='text' placeholder='Age' />
-                        <br />
-                        <label for="myfile">Select a Image: </label>
-                        <input type="file" id="myfile" name="myfile" />
-                        <br />
-                        <button onClick={() => setIsAddBirthdayShown(false)}>Close</button>
-                    </form>
-                </div>
-
-            </div>
+  return (
+    <div className={showHideClassName}>
+      <div className="modal">
+        <p className="modal-title">Who's Birthday?</p>
+        <div className="form-container">
+          <form className="birthday-form">
+            <input type="text" placeholder="Name" />
+            <br />
+            <input type="text" placeholder="Age" />
+            <br />
+            <label for="myfile">Select a Image: </label>
+            <input type="file" id="myfile" name="myfile" />
+            <br />
+            <button onClick={() => setIsAddBirthdayShown(false)}>Close</button>
+          </form>
         </div>
-
-    )
-
+      </div>
+    </div>
+  );
 }
+
+function AddBirthdayButton({ setIsAddBirthdayShown }) {
+  return (
+    <div>
+      <button className="btn" onClick={() => setIsAddBirthdayShown(true)}>
+        Add Birthday
+      </button>
+    </div>
+  );
+}
+
+export { AddBirthdayModal, AddBirthdayButton };
--- src/App.js
+++ src/App.js
@@ -1,47 +1,62 @@
-import { useState } from 'react';
-import './App.css';
-import Birthdays from './Birthdays';
-import BirthayData from './BirthdayData'
-import AddBirthdayModal from './AddBirthdayModal';
-
-
+import { useState } from "react";
+import "./App.css";
+import Birthdays from "./Birthdays";
+import BirthayData from "./BirthdayData";
+import { AddBirthdayModal, AddBirthdayButton } from "./AddBirthdayModal";
 
 function App() {
-//Keeping track of all birthdays
-  const [allBirthdays, setAllBirthdays] = useState([...BirthayData])
+  //Keeping track of all birthdays
+  const [allBirthdays, setAllBirthdays] = useState([...BirthayData]);
   //keeping track if add birthday modal is displayed
-  const [isAddBirthdayShown, setIsAddBirthdayShown] = useState(false)
+  const [isAddBirthdayShown, setIsAddBirthdayShown] = useState(false);
 
-//Delete selected birthday by index and reset new birthday array.
+  //Delete selected birthday by index and reset new birthday array.
   const DeleteBirthday = (indx) => {
-    allBirthdays.splice(indx, 1)
-    setAllBirthdays([...allBirthdays])
-  }
+    const updatedBirthdays = [...allBirthdays];
+    updatedBirthdays.splice(indx, 1);
+    setAllBirthdays(updatedBirthdays);
+  };
 
   return (
     <>
-    <AddBirthdayModal isAddBirthdayShown={isAddBirthdayShown} setIsAddBirthdayShown={setIsAddBirthdayShown} />
+      <AddBirthdayModal
+        setIsAddBirthdayShown={setIsAddBirthdayShown}
+        isAddBirthdayShown={isAddBirthdayShown}
+      />
       <div className="birthdays-container">
-        <h2 style={{ textAlign: 'center', fontFamily: 'cursive' }}>{allBirthdays.length} Birthdays Today</h2>
-        <div style={{ minHeight: '550px', height: "fit-content" }}>
-          {allBirthdays.length >= 1 ? allBirthdays.map((user, index) => {
-            return <Birthdays
-              key={user.id}
-              selfie={user.image}
-              name={user.Name}
-              age={user.AgeTurning}
-              deleteBirthday={DeleteBirthday}
-              index={index}
-            />
-          }) : null}
-
+        <h2 style={{ textAlign: "center", fontFamily: "cursive" }}>
+          {allBirthdays.length} Birthdays Today
+        </h2>
+        <div style={{ minHeight: "550px", height: "fit-content" }}>
+          {allBirthdays.length >= 1
+            ? allBirthdays.map((user, index) => {
+                return (
+                  <Birthdays
+                    key={user.id}
+                    selfie={user.image}
+                    name={user.Name}
+                    age={user.AgeTurning}
+                    deleteBirthday={DeleteBirthday}
+                    index={index}
+                  />
+                );
+              })
+            : null}
         </div>
-
       </div>
 
-      <div style={{ display: 'flex', height: '30px', marginTop: '10px', justifyContent: 'center' }}>
-        <button className='btn' onClick={() => setAllBirthdays([])}>Clear</button>
-        <button className='btn' onClick={() => setIsAddBirthdayShown(true)}>Add Birthday</button>
+      <div
+        style={{
+          display: "flex",
+          height: "30px",
+          marginTop: "10px",
+          justifyContent: "center",
+        }}
+      >
+        <button className="btn" onClick={() => setAllBirthdays([])}>
+          Clear
+        </button>
+        <AddBirthdayButton setIsAddBirthdayShown={setIsAddBirthdayShown} />
       </div>
     </>
   );
